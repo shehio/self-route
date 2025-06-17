79012c58c06f3a455859147b5dbbaa4b8658ac12
shehio <shehabyasser@gmail.com>
2025-06-17 14:21:14 -0400

    Added ecs cluster/service/taskdef

--- terraform/main.tf
+++ terraform/main.tf
@@ -151,3 +151,74 @@ resource "aws_security_group" "allow_all_security_group" {
     Name = "SelfRouteSecurityGroup"
   }
 }
+
+# ECS Cluster
+resource "aws_ecs_cluster" "self_route_cluster" {
+  name = "self-route-cluster"
+}
+
+# IAM Role for ECS Tasks
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name = "ecs-task-execution-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "ecs-tasks.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_execution" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+# Task Definition
+resource "aws_ecs_task_definition" "nginx_task" {
+  family                   = "nginx-task"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = 1024  # 1 vCPU
+  memory                   = 3072  # 3GB
+  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
+
+  runtime_platform {
+    operating_system_family = "LINUX"
+    cpu_architecture       = "X86_64"
+  }
+
+  container_definitions = jsonencode([{
+    name      = "nginx"
+    image     = "nginx:latest"
+    cpu       = 1024
+    memory    = 3072
+    essential = true
+    portMappings = [{
+      containerPort = 80
+      hostPort      = 80
+      protocol      = "tcp"
+      appProtocol   = "http"
+      name          = "nginx-container-80-tcp"
+    }]
+  }])
+}
+
+# ECS Service
+resource "aws_ecs_service" "nginx_service" {
+  name            = "nginx-service"
+  cluster         = aws_ecs_cluster.self_route_cluster.id
+  task_definition = aws_ecs_task_definition.nginx_task.arn
+  desired_count   = 2
+  launch_type     = "FARGATE"
+
+  network_configuration {
+    subnets          = concat(aws_subnet.public[*].id, aws_subnet.private[*].id)
+    security_groups  = [aws_security_group.allow_all_security_group.id]
+    assign_public_ip = true
+  }
+}
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,11 +1,44 @@
 output "vpc_id" {
-  value = aws_vpc.self_route_vpc.id
+  description = "ID of the VPC"
+  value       = aws_vpc.self_route_vpc.id
 }
 
 output "public_subnet_ids" {
-  value = aws_subnet.public[*].id
+  description = "List of public subnet IDs"
+  value       = aws_subnet.public[*].id
 }
 
 output "private_subnet_ids" {
-  value = aws_subnet.private[*].id
+  description = "List of private subnet IDs"
+  value       = aws_subnet.private[*].id
+}
+
+output "ecs_cluster_name" {
+  description = "Name of the ECS cluster"
+  value       = aws_ecs_cluster.self_route_cluster.name
+}
+
+output "ecs_cluster_arn" {
+  description = "ARN of the ECS cluster"
+  value       = aws_ecs_cluster.self_route_cluster.arn
+}
+
+output "ecs_service_name" {
+  description = "Name of the ECS service"
+  value       = aws_ecs_service.nginx_service.name
+}
+
+output "task_definition_arn" {
+  description = "ARN of the task definition"
+  value       = aws_ecs_task_definition.nginx_task.arn
+}
+
+output "task_definition_family" {
+  description = "Family of the task definition"
+  value       = aws_ecs_task_definition.nginx_task.family
+}
+
+output "task_definition_revision" {
+  description = "Revision of the task definition"
+  value       = aws_ecs_task_definition.nginx_task.revision
 }
